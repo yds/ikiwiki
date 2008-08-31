4f2ab4db71e2afc3bf9a71bcdffdf56d820489bc
David Bremner <bremner@unb.ca>
2008-08-31 16:41:15 -0300

    work on postal-accept filter to add message to wiki

--- filters/postal-accept.pl
+++ filters/postal-accept.pl
@@ -1,16 +1,22 @@
 #!/usr/bin/perl
 
-use Email::Folder;
+# Take message on stdin, write to appropiate comment folder, 
+# update wiki
+
 use Email::LocalDelivery;
+use Email::Filter;
 
 use Convert::YText qw(decode_ytext encode_ytext);
 
 # we need at least version 2.54 of IkiWiki for the new config api
 BEGIN { require IkiWiki; die unless ($IkiWiki::version >= 2.54) }
 
+use IkiWiki;
 use IkiWiki::Setup;
 use Getopt::Long;
 use Carp;
+
+my $folder_ext=".mbox";
 
 my $config_file=undef;
 
@@ -18,54 +24,40 @@ GetOptions('config=s'=>\$config_file);
 
 die "configuration file is mandatory" unless ($config_file);
 
-my %config=IkiWiki::Setup::load($config_file);
+%config=IkiWiki::defaultconfig();
 
-my $queue=$config{queue} || croak("missing queue location");
+IkiWiki::Setup::load($config_file);
 
-my $folder=Email::Folder->new($path,reader=>'Email::Folder::maildir') 
-    || croak("mailbox could not be opened");
+$message=Email::Filter->new();
 
-for my $messages  ($folder->messages){
-
-    my $to=$message->to;
-    if ($to =~ m/$prefix($Convert::YText::valid_rex)/){
-	my $page=decode_ytext($1);
-	
-	# sanity check the page path
-	
-	# check if comments folder exists, create if needed
-
-	# write the message to the folder
-
-	$msg_file= $comment_folder->write($message);
-
-	# update vcs, copied from Ikiwiki::Plugins::attachment
+my $to=$message->to;
+if ($to =~ m/$prefix($Convert::YText::valid_rex)/){
+    my $page=decode_ytext($1);
     
-	if ($config{rcs}) {
-	    IkiWiki::rcs_add($filename);
-	    IkiWiki::disable_commit_hook();
-	    IkiWiki::rcs_commit($filename, gettext("attachment upload"),
-				IkiWiki::rcs_prepedit($filename),
-				$session->param("name"), $ENV{REMOTE_ADDR});
-	    IkiWiki::enable_commit_hook();
-	    IkiWiki::rcs_update();
-	}
+    die("page ".$page." does not exist") if !($pagesources{$page});
+    
+    my $comments_folder=$page."/comments".$folder_ext;
 
-	# tag for deletion
-	
-	push (@must_die, $message->message-id);
-	
-	$folder->delete($message);
+    # write the message to the comment
 
+    my ($delivered) = Email::LocalDeliver($config{srcdir}."/".$comments_folder);
+    
+    die ("delivery failed") if (!defined ($delivered));
+    
+    # update vcs, copied from Ikiwiki::Plugins::attachment
+    
+    if ($config{rcs}) {
+	IkiWiki::rcs_add($delivered);
+	IkiWiki::disable_commit_hook();
+	IkiWiki::rcs_commit($delivered, gettext("postal delivery"),
+			    IkiWiki::rcs_prepedit($delivered));
+	IkiWiki::enable_commit_hook();
+	IkiWiki::rcs_update();
     }
 
     
     # refresh wiki
     IkiWiki::refresh();
     IkiWiki::saveindex();
- 
-    # make regex for message-id
-
-    # called delete_message from Email::Delete
-}
+} 
 
