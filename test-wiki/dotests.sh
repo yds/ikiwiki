ebe5398f498dbe5ac37bc3dd2298b50f31c97cf7
David Bremner <bremner@unb.ca>
2008-09-07 13:12:51 -0300

    modify setup for vcs (git) enable test-wiki

---
+++ test-wiki/dotests.sh
@@ -0,0 +1,10 @@
+#!/bin/sh 
+rm -rf test-src test-repo.git /tmp/postal-test
+cp -ar in test-src
+
+ikiwiki-makerepo git test-src test-repo.git
+ikiwiki --rebuild --setup test.setup
+perl ../filters/postal-accept.pl -c test.setup < test1.eml
+perl ../filters/postal-accept.pl -c test.setup < test2.eml
+perl ../filters/postal-accept.pl -c test.setup < test3.eml
+ikiwiki --setup test.setup --refresh
